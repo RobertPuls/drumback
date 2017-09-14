5b6941823ee085cb4aced8d70b5aace9ad41c404
Robert Puls <rpu1s@sbcglobal.net>
2017-09-13 19:25:17 -0600

    test

--- app.js
+++ app.js
@@ -12,7 +12,39 @@ app.use(bodyParser.urlencoded({
 
 app.use(cors());
 
-app.use("/", routes);
+var Gpio = require('pigpio').Gpio;
+var router = express.Router();
+
+var motor = new Gpio(4, {
+  mode: Gpio.OUTPUT
+});
+var arr = [1, 1, 0, 1, 1];
+var pulseWidth = 500;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function test() {
+  if (arr[i] == 1) {
+    motor.servoWrite(pulseWidth);
+    await sleep(100);
+    pulseWidth = 1000;
+
+    motor.servoWrite(pulseWidth);
+    await sleep(100);
+    pulseWidth = 500;
+  } else {
+    await sleep(200);
+  }
+}
+
+router.get('/', function(req, res, next) {
+  console.log("HEY LISTEN")
+  test();
+});
+
+// app.use("/", routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
