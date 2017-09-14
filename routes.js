29fe9793491a3c28b814cccbeb76fa9eae2d5034
Robert Puls <rpu1s@sbcglobal.net>
2017-09-14 15:23:49 -0600

    changes servo speed

--- routes.js
+++ routes.js
@@ -23,30 +23,26 @@ async function alternate(arr) {
     console.log("arr", arr[0][i]);
     if (arr[0][i] == 1) {
       motor.servoWrite(pulseWidth);
-      console.log("right");
-      await sleep(70);
+      await sleep(75);
       pulseWidth = 1000;
 
       motor.servoWrite(pulseWidth);
-      console.log("left");
-      await sleep(70);
+      await sleep(75);
       pulseWidth = 500;
     } else {
-      await sleep(140);
+      await sleep(150);
     }
 
     if (arr[1][i] == 1) {
       motor1.servoWrite(pulseWidth);
-      console.log("right");
-      await sleep(70);
+      await sleep(75);
       pulseWidth = 1000;
 
       motor1.servoWrite(pulseWidth);
-      console.log("left");
-      await sleep(70);
+      await sleep(75);
       pulseWidth = 500;
     } else {
-      await sleep(140);
+      await sleep(150);
     }
 
   }
