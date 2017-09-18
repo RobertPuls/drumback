4e94920183a91aecb21fdd5a8805485263ccc595
Robert Puls <rpu1s@sbcglobal.net>
2017-09-18 15:57:26 -0600

    bug fix

--- routes/beats.js
+++ routes/beats.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const queries = require("../db/queries");
 
-router.get('/' (req, res) => {
+router.get('/', (req, res) => {
   queries.getAllBeats().then(beats => res.json({
     "beats": beats
   }));
 });
 
-router.post('/' (req, res) => {
+router.post('/', (req, res) => {
   console.log(req.body.pattern);
   queries.create(req.body).then(beat => res.json({
     'beats': beat
